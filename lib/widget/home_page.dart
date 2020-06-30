f2efd268eb9cecdfc239594a1fd59fb10aa4ba95
Zeph <zephaniahsefuoduori@gmail.com>
2020-06-30 04:10:48 +0300

    Add rounded button on the app bar

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterflighsearch/widget/button/rounded_button.dart';
 
 import 'air_asia_bar.dart';
 
@@ -8,7 +9,41 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: <Widget>[AirAsiaBar(height: 210)],
+        children: <Widget>[
+          AirAsiaBar(height: 210.0),
+          Positioned.fill(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).padding.top + 40.0),
+              child: new Column(
+                children: <Widget>[
+                  _buildButtonsRow(),
+                  Container(), //TODO: Implement a card
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtonsRow() {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          new RoundedButton(
+            text: "ONE WAY",
+          ),
+          new RoundedButton(
+            text: "ROUND",
+          ),
+          new RoundedButton(
+            text: "MULTICITY",
+            selected: true,
+          )
+        ],
       ),
     );
   }
